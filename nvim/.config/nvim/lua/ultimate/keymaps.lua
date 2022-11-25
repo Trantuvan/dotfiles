19f4ce5d6d3b529843853222e4b00bce650f9148
trantuvan <trantuvan.kan@gmail.com>
2022-11-25 22:31:01 +0700

    fix keymaps

--- nvim/.config/nvim/lua/ultimate/keymaps.lua
+++ nvim/.config/nvim/lua/ultimate/keymaps.lua
@@ -1,7 +1,7 @@
---Shorten function name
+-- Shorten function name
 local keymap = vim.keymap.set
---Silent keymap option
-local opts = {silent = true}
+-- Silent keymap option
+local opts = { silent = true }
 
 --Remap space as leader key
 keymap("", "<Space>", "<Nop>", opts)
@@ -15,60 +15,60 @@ vim.g.mapleader = " "
 --   term_mode = "t",
 --   command_mode = "c",
 
---Normal
---Better window navigation
+-- Normal --
+-- Better window navigation
 keymap("n", "<C-h>", "<C-w>h", opts)
 keymap("n", "<C-j>", "<C-w>j", opts)
 keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
 
---Resize with arrows
-keymap("n", "<C-Up>", ":resize +2<CR>", opts)
-keymap("n", "<C-Down>", ":resize -2<CR>", opts)
+-- Resize with arrows
+keymap("n", "<C-Up>", ":resize -2<CR>", opts)
+keymap("n", "<C-Down>", ":resize +2<CR>", opts)
 keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
 keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
---Navigate buffers
+-- Navigate buffers
 keymap("n", "<S-l>", ":bnext<CR>", opts)
-keymap("n", "<S-l>", ":bprevious<CR>", opts)
+keymap("n", "<S-h>", ":bprevious<CR>", opts)
 
---Clear highlights
+-- Clear highlights
 keymap("n", "<leader>h", "<cmd>nohlsearch<CR>", opts)
 
---Close buffers
-keymap("n", "<S-q>", "<cmd>bdelete!<CR>", opts)
+-- Close buffers
+keymap("n", "<S-q>", "<cmd>Bdelete!<CR>", opts)
 
---Better patse
-keymap("v", "p", '"_dP"', opts)
+-- Better paste
+keymap("v", "p", '"_dP', opts)
 
---Insert
---Press jk fast to enter
+-- Insert --
+-- Press jk fast to enter
 keymap("i", "jk", "<ESC>", opts)
 
---Visual
---Stay in indent mode
-keymap("v", "<","<gv", opts)
-keymap("v", ">",">gv", opts)
+-- Visual --
+-- Stay in indent mode
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
 
---Plugins
+-- Plugins --
 
---NvimTree
+-- NvimTree
 keymap("n", "<leader>e", ":NvimTreeToggle<CR>", opts)
 
---Telescope
+-- Telescope
 keymap("n", "<leader>ff", ":Telescope find_files<CR>", opts)
-keymap("n", "<leader>ft", ":Telescope find_grep<CR>", opts)
+keymap("n", "<leader>ft", ":Telescope live_grep<CR>", opts)
 keymap("n", "<leader>fp", ":Telescope projects<CR>", opts)
 keymap("n", "<leader>fb", ":Telescope buffers<CR>", opts)
 
---Git
+-- Git
 keymap("n", "<leader>gg", "<cmd>lua _LAZYGIT_TOGGLE()<CR>", opts)
 
---Comment
+-- Comment
 keymap("n", "<leader>/", "<cmd>lua require('Comment.api').toggle.linewise.current()<CR>", opts)
-keymap("x", "<leader>/", "<ESC>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>", opts)
+keymap("x", "<leader>/", '<ESC><CMD>lua require("Comment.api").toggle.linewise(vim.fn.visualmode())<CR>')
 
---DAP
+-- DAP
 keymap("n", "<leader>db", "<cmd>lua require'dap'.toggle_breakpoint()<cr>", opts)
 keymap("n", "<leader>dc", "<cmd>lua require'dap'.continue()<cr>", opts)
 keymap("n", "<leader>di", "<cmd>lua require'dap'.step_into()<cr>", opts)
